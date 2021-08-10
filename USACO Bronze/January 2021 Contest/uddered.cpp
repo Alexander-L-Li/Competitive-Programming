0b7a2cb55ebc0d6cb3ef25fab1bdc1f4c88e1bc7
Alexander Li <alexli01890@gmail.com>
2021-08-10 02:49:08 -0400

    Create uddered.cpp

---
+++ USACO Bronze/January 2021 Contest/uddered.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+
+void setIO(string name = ""){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    string alp, word; cin >> alp >> word;
+    int pointer = 0; int ans = 0;
+    while(pointer < word.length()){
+        F0R(a, alp.length()){
+            if(alp[a] == word[pointer]){
+                pointer++;
+            }
+        }
+        ans++;
+    }
+    cout << ans;
+}
+
+int main(){
+    setIO();
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
