aefa1d233c2a72ab765dfbd0fe1ecf2f45c967d7
Alexander Li <alexli01890@gmail.com>
2021-08-11 03:40:05 -0400

    Create revegetation.cpp

---
+++ USACO Bronze/February 2019 Contest/revegetation.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tll;
+typedef tuple<string, string, string> ts;
+typedef tuple<char, char, char> tc;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef vector<ti> vti;
+typedef vector<bool> vb;
+typedef vector<ts> vts;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef multiset<char> mc;
+typedef queue<char> qc;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+void setIO(string name = "revegetate"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    int n, m; cin >> n >> m;
+    vpi p; int arr[n];
+    F0R(a, m){
+        int b, c;
+        cin >> b >> c;
+        p.pb(mp(min(b,c), max(b,c)));
+    }
+    for(int a=1; a<=n; a++){
+        int b; 
+        for(b=1; b<=4; b++){
+            bool done = false;
+            for(int c=0; c<m; c++){
+                if(p[c].second == a && arr[p[c].first] == b){
+                    done = true;
+                }   
+            }
+            if(!done){
+                break;
+            }
+        }
+        arr[a] = b;
+        cout << b;
+    }
+}
+
+int main(){
+    setIO();
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
