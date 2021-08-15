676c0004391e7417d234aa3efaccacfc84b6873a
Alexander Li <alexli01890@gmail.com>
2021-08-15 18:07:10 -0400

    Create mocha_and_math.cpp

---
+++ CodeForces/CF Round #738 (Div. 2)/mocha_and_math.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FOR(a, b, c) for (ll a=(b); a<(c); a++)
+#define F0R(i, a) for (ll i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+
+void setIO(string name = ""){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    int n; cin >> n;
+    int arr[n];
+    F0R(a, n){
+        cin >> arr[a];
+    }
+    int ans = arr[0];
+    FOR(a, 1, n){
+        ans &= arr[a];
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+    setIO();
+    int t = 1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
