0610d2293f6ec6a4d483fc59a02cbb2b18f517ad
Alexander Li <alexli01890@gmail.com>
2021-08-19 01:50:40 -0400

    Create clockwise_fence.cpp

---
+++ USACO Bronze/February 2021 Contest/clockwise_fence.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tll;
+typedef tuple<string, string, string> ts;
+typedef tuple<char, char, char> tc;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<ti> vti;
+typedef vector<bool> vb;
+typedef vector<ts> vts;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef multiset<char> mc;
+typedef queue<char> qc;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+void setIO(string name = ""){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int check(char x, char y){
+    int a = 90, b = -90;
+    if(x == 'W' && y == 'S'){
+        return b;
+    } else if(x == 'S' && y == 'E'){
+        return b;
+    } else if(x == 'E' && y == 'N'){
+        return b;
+    } else if(x == 'N' && y == 'W'){
+        return b;
+    } else {
+        return a;
+    }
+}
+
+void solve(){
+    string s; cin >> s;
+    int ans = 0;
+    F0R(a, s.length()-1){
+        if(s[a] != s[a+1]){
+            ans += check(s[a], s[a+1]);
+        }
+    }
+    if(ans < 0){
+        cout << "CCW" << endl;
+    } else {
+        cout << "CW" << endl;
+    }
+}   
+
+int main(){
+    setIO();
+    int t = 1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
