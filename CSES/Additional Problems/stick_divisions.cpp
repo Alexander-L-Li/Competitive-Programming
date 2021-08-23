bd6b8fe8b9c553e95f07fef16cbbcf022aad87bf
Alexander Li <alexli01890@gmail.com>
2021-08-23 14:38:07 -0400

    Create stick_divisions.cpp

---
+++ CSES/Additional Problems/stick_divisions.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tll;
+typedef tuple<string, string, string> ts;
+typedef tuple<char, char, char> tc;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<ti> vti;
+typedef vector<bool> vb;
+typedef vector<ts> vts;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef multiset<char> mc;
+typedef queue<char> qc;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define nl endl
+
+void solve(){
+    int x, n; cin >> x >> n;
+    priority_queue<int, vi, greater<int>> p;
+    F0R(a, n){
+        int b; cin >> b;
+        p.push(b);
+    }   
+    ll ans = 0; ll sum = 0;
+    F1R(a, n){
+        int b = p.top(); p.pop();
+        int c = p.top(); p.pop();
+        p.push(b+c);
+        ans += b+c;
+    }
+    cout << ans;
+}
+
+
+int main(){
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
