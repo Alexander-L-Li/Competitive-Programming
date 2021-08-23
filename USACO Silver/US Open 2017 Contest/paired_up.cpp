6fc5fd2ff37a68ecd6172956351e84c40bde8d70
Alexander Li <alexli01890@gmail.com>
2021-08-22 22:57:33 -0400

    Create paired_up.cpp

---
+++ USACO Silver/US Open 2017 Contest/paired_up.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tll;
+typedef tuple<string, string, string> ts;
+typedef tuple<char, char, char> tc;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<ti> vti;
+typedef vector<bool> vb;
+typedef vector<ts> vts;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef multiset<char> mc;
+typedef queue<char> qc;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+void setIO(string name = "pairup"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main(){
+    setIO();
+    int n; cin >> n;
+    vpll v;
+    F0R(a, n){
+        int x, y; cin >> x >> y;
+        v.pb(mp(y, x));
+    }
+    sort(all(v));
+    ll ans = 0; int p1 = 0, p2 = n-1;
+    int c1 = 0, c2 = 0;
+    while(p1 <= p2){
+        ans = max(ans, v[p1].first + v[p2].first);    
+        c1++; c2++;
+        if(c1 > v[p1].second){
+            p1++;
+            c1 = 0;
+        }
+        if(c2 > v[p2].second){
+            p2--;
+            c2 = 0;
+        }   
+    }   
+    cout << ans;
+}
