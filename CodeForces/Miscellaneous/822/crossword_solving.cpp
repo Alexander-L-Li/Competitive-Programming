c35923e06f490b7f489cd2b97296ece6b3ce78ec
Alexander Li <alexli01890@gmail.com>
2021-08-23 21:05:43 -0400

    Create crossword_solving.cpp

---
+++ CodeForces/Miscellaneous/822/crossword_solving.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+typedef tuple<int, int, int> ti;
+typedef tuple<ll, ll, ll> tll;
+typedef tuple<string, string, string> ts;
+typedef tuple<char, char, char> tc;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<ti> vti;
+typedef vector<bool> vb;
+typedef vector<ts> vts;
+typedef multiset<int> msi;
+typedef multiset<ll> msll;
+typedef multiset<string> mss;
+typedef multiset<char> mc;
+typedef queue<char> qc;
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<string> qs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<string> ss;
+typedef set<char> sc;
+
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define popb pop_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define beg begin
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+void solve(){
+    int n, m; cin >> n >> m;
+    string s, s2; cin >> s >> s2;
+    int ans = 0; 
+    vector<vector<int>> v(1000); int pointer = 0;
+    for(int a=0; a<=m-n; a++){
+        int count = 0;
+        F0R(b, n){
+            if(s2[a + b] == s[b]){
+                count++;
+                v[a].pb(b);
+            }
+        }
+        if(count > ans){
+            pointer = a;
+        }
+        ans = max(count, ans);
+    }
+    cout << n - ans << endl; 
+    F0R(a, n){
+        bool done = false;
+        F0R(b, v[pointer].size()){
+            if(v[pointer][b] == a){
+                done = true;
+            }
+        }
+        if(!done){
+            cout << a+1 << " ";
+        }
+    }
+}
+
+int main(){
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
