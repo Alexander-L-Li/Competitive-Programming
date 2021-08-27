f70e9be0ba1e8556145a777b678916b678f913c3
Alexander Li <alexli01890@gmail.com>
2021-08-27 16:09:24 -0400

    Create supercentral.cpp

---
+++ CodeForces/Miscellaneous/165/supercentral.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<pc> vpc;
+
+template<class T> using ms = multiset<T>;
+template<class T> using tup = tuple<T, T, T>;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAXN = 1e9;
+
+void solve(){
+    int n; cin >> n;
+    vpi v; 
+    F0R(a, n){
+        int x, y; cin >> x >> y;
+        v.pb(mp(x, y));
+    }
+    int ans = 0;
+    F0R(a, n){
+        bool c1 = false, c2 = false, c3 = false, c4 = false;
+        F0R(b, n){
+            if(v[a].first > v[b].first && v[a].second == v[b].second){
+                c1 = true;
+            } else if(v[a].first < v[b].first && v[a].second == v[b].second){
+                c2 = true;
+            } else if(v[a].second > v[b].second && v[a].first == v[b].first){
+                c3 = true;
+            } else if(v[a].second < v[b].second && v[a].first == v[b].first){
+                c4 = true;
+            }
+        }
+        if(c1 && c2 && c3 && c4){
+            ans++;
+        }
+    }
+    cout << ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
