2172d551804e7fc6e33d2c56c42473c6feea2be3
Alexander Li <alexli01890@gmail.com>
2021-08-31 20:17:18 -0400

    Create angrycows.cpp

---
+++ USACO Silver/January 2016 Contest/angrycows.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<pc> vpc;
+
+template<class T> using ms = multiset<T>;
+template<class T> using tup = tuple<T, T, T>;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAXN = 1e5;
+
+void setIO(string name = "angry"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(name.size()){
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main(){
+    setIO();
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    F0R(a, n){
+        cin >> arr[a];
+    }
+    sort(arr, arr+n);
+    ll lo = 0, hi = 500000000;
+    while(lo != hi){
+        ll mid = (lo + hi)/2;
+        int c = 0, a = 0;
+        while(a < n){
+            c++;
+            int curr = a+1;
+            while(curr < n && arr[curr] - arr[a] <= 2*mid){
+                curr++;
+            }
+            a = curr;
+        }
+        if(c <= k){
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    cout << lo;
+}
