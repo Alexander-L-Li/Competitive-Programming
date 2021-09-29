b21ccc4eb1e1ac1319e1fec1b007b347441fcdf7
Alexander Li <alexli01890@gmail.com>
2021-09-28 23:36:24 -0400

    Update high_card_wins.cpp

--- USACO Silver/December 2015 Contest/high_card_wins.cpp
+++ USACO Silver/December 2015 Contest/high_card_wins.cpp
@@ -1,98 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
 #include <algorithm>
-#include <string>
-#include <set>
-#include <queue>
-#include <map>
+#include <vector>
 
 using namespace std;
 
-typedef long double ld;
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll,ll> pll;
-typedef pair<char, char> pc;
-typedef pair<string, string> ps;
-typedef tuple<int, int, int> ti;
-typedef tuple<ll, ll, ll> tll;
-typedef tuple<string, string, string> ts;
-typedef tuple<char, char, char> tc;
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef vector<ll> vll;
-typedef vector<pi> vpi;
-typedef vector<ps> vps;
-typedef vector<pll> vpll;
-typedef vector<ti> vti;
-typedef vector<bool> vb;
-typedef vector<ts> vts;
-typedef multiset<int> msi;
-typedef multiset<ll> msll;
-typedef multiset<string> mss;
-typedef multiset<char> mc;
-typedef queue<char> qc;
-typedef queue<int> qi;
-typedef queue<ll> qll;
-typedef queue<string> qs;
-typedef set<int> si;
-typedef set<ll> sll;
-typedef set<string> ss;
-typedef set<char> sc;
-
-#define FOR(a, b, c) for (int a=(b); a<(c); a++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F1R(i, a) for (int i=1; i<(a); i++)
-#define mp make_pair
-#define mt make_tuple
-#define pb push_back
-#define popb pop_back
-#define f first
-#define sec second
-#define ub upper_bound
-#define lb lower_bound
-#define beg begin
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-
-void setIO(string name = "highcard"){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    if(name.size()){
-        freopen((name + ".in").c_str(), "r", stdin);
-        freopen((name + ".out").c_str(), "w", stdout);
-    }
-}
-
 int main(){
-    setIO();
+    freopen("highcard.in", "r", stdin);
+    freopen("highcard.out", "w", stdout);
     int n; cin >> n;
-    vi v, bes;
-    priority_queue<int, vi, greater<int>> v2;
-    F0R(a, n){
-        int b; cin >> b;
-        v.pb(b);
-        v2.push(b);
+    vector<int> v(n*2);
+    vector<int> v2(n);
+    for(int a=0; a<n; a++){
+        cin >> v2[a];
+        v[v2[a]-1]++;
     }
-    F1R(a, (n*2)+1){
-        if(a != v2.top()){
-            bes.pb(a);
+    vector<int> e, b;
+    for(int a=0; a<n*2; a++){
+        if(v[a] == 0){
+            b.push_back(a+1);
         } else {
-            v2.pop();
+            e.push_back(a+1);
         }
     }
+    sort(e.begin(), e.end());
+    sort(b.begin(), b.end());
     int ans = 0;
-    sort(all(v));
-    sort(all(bes));
-    while(!v.empty() && !bes.empty()){
-        if(bes[bes.size()-1] > v[v.size()-1]){
-            bes.erase(bes.begin() + bes.size()-1);
-            v.erase(v.begin() + v.size()-1);
+    while(!e.empty() && !b.empty()){
+        if(b[0] > e[0]){
+            b.erase(b.begin());
+            e.erase(e.begin());
             ans++;
         } else {
-            bes.erase(bes.begin());
-            v.erase(v.begin() + v.size()-1);
+            b.erase(b.begin());
         }
     }
     cout << ans;
