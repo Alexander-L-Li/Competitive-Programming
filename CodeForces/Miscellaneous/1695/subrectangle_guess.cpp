fb56547dda444835621cc78a250fc307f686893b
Alexander Li <alexli01890@gmail.com>
2022-06-18 23:26:02 -0400

    Create subrectangle_guess.cpp

---
+++ CodeForces/Miscellaneous/1695/subrectangle_guess.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<char, char> pc;
+typedef pair<string, string> ps;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<ps> vps;
+typedef vector<pll> vpll;
+typedef vector<pc> vpc;
+
+template<class T> using ms = multiset<T>;
+template<class T> using tup = tuple<T, T, T>;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define FOR(a, b, c) for (int a=(b); a<(c); a++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F1R(i, a) for (int i=1; i<(a); i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(i,a) for (auto i=a; i<a; i++)
+
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define f first
+#define sec second
+#define ub upper_bound
+#define lb lower_bound
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAXN = 1e5;
+
+void solve(){
+    int h, k; cin >> h >> k;
+    int arr[h][k];
+    int maxN = INT_MIN;
+    int h2, k2;
+    F0R(a, h){
+        F0R(b, k){
+            cin >> arr[a][b];
+            maxN = max(maxN, arr[a][b]);
+        }
+    }
+    F0R(a, h){
+        bool check = false;
+        F0R(b, k){
+            if(maxN == arr[a][b]){
+                h2 = a;
+                k2 = b;
+                check = true;
+                break;
+            }
+        }
+        if(check){
+            break;
+        }
+    }
+    int ans1 = max(min(h2+1, h), h-h2);
+    int ans2 = max(min(k2+1, k), k-k2);
+    cout << ans1*ans2 << endl;
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int t = 1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
